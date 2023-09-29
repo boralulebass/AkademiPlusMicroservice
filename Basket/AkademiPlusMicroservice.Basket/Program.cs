0cbbba0448decf8d98a6cb92ee19ade74d20f4e7
Bora Lülebaş <126796068+boralulebass@users.noreply.github.com>
2023-09-29 15:53:06 +0300

    BasketService

--- Basket/AkademiPlusMicroservice.Basket/Program.cs
+++ Basket/AkademiPlusMicroservice.Basket/Program.cs
@@ -1,37 +1,27 @@
-using AkademiPlusMicroservice.Discount.Context;
-using AkademiPlusMicroservice.Discount.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using System.IdentityModel.Tokens.Jwt;
-using System.Reflection;
-
-var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
-
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt=>
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
 {
     opt.Authority = builder.Configuration["IdentityServerUrl"];
-    opt.Audience = "resource_discount";
+    opt.Audience = "resource_basket";
     opt.RequireHttpsMetadata = false;
 });
-builder.Services.AddControllers(opt=>
+builder.Services.AddControllers(opt =>
 {
     opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DapperContext>();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddScoped<IDiscountCouponService, DiscountCouponService>();
-builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
